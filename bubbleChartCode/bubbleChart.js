13a1c903c8cb8b4b032bd6947a0f2b0e1e4f659a
Manuela <mnzali13@gmail.com>
2024-10-18 10:49:13 -0500

    third commit

--- bubbleChartCode/bubbleChart.js
+++ bubbleChartCode/bubbleChart.js
@@ -1,12 +1,12 @@
 // Build the metadata panel
-d3.json("output.json").then((output) => {
+d3.json("https://raw.githubusercontent.com/manuelanzali/Project-3/refs/heads/main/bubbleChartCode/output.json").then((output) => {
  //Add a variable for the country from the country array
  const countries = output.country;
 
  //Create the dropdown menu
  const dropdown = d3.select("#selDataset")
  countries.forEach(country => {
-    dropdown.append("option").text(country.name);
+    dropdown.append("option").text(country);
   });
 
   //create an initial bubble chart with the first country's data
@@ -17,7 +17,7 @@ d3.json("output.json").then((output) => {
   // Function to create the bubble chart
 function createBubbleChart(countryName) {
     // Filter the data for the selected country
-    d3.json('output.json').then(function(output) {
+    d3.json('https://raw.githubusercontent.com/manuelanzali/Project-3/refs/heads/main/bubbleChartCode/output.json').then(function(output) {
         let countryData;
         for (let i=0; i < output.country.length; i++) {
             if (output.country[i] === country) {
@@ -30,13 +30,13 @@ function createBubbleChart(countryName) {
         // Prepare data for the bubble chart, limit to 6 songs
         const bubbleLayout = {
             title: "Song metrics by Country",
-            x: output.name.slice(0,5).map(songName => output.name), // Adjust based on your data structure
-            y: output.liveliness.slice(0,5).map(liveliness => output.liveliness), // Adjust based on your data structure
-            text: output.name.slice(0,5).map(songName => output.name), // Adjust based on your data structure
+            x: output.name.slice(0,5).map(songName => output.name),
+            y: output.liveliness.slice(0,5).map(liveliness => output.liveliness), 
+            text: output.name.slice(0,5).map(songName => output.name), 
             mode: 'markers',
             marker: {
-                size: output.liveliness.slice(0,6).map(liveliness => liveliness.sizeValue), // Adjust based on your data structure
-                color: output.name.slice(0,6).map(songName => output.name), // Adjust based on your data structure
+                size: output.liveliness.slice(0,6).map(liveliness => liveliness.sizeValue),
+                color: output.name.slice(0,6).map(songName => output.name),
                 colorscale: "Inferno"
             }
         };
@@ -46,7 +46,7 @@ function createBubbleChart(countryName) {
     });
 }
 
-// Function to handle dropdown change
+// Function to change the dropdown
 function optionChanged(selectedCountry) {
     createBubbleChart(selectedCountry);
 }
